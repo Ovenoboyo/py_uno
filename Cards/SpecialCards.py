8ff375f7117347d3673faf9898c19ccb820abd49
adi1069 <deshpande.aditya16@gmail.com>
2020-01-29 16:18:22 +0530

    Predefine cards in deck
    Signed-off-by: adi1069 <deshpande.aditya16@gmail.com>

--- Cards/SpecialCards.py
+++ Cards/SpecialCards.py
@@ -3,6 +3,7 @@ class Reverse(object):
         self.color = color
         self.type = "SPECIAL"
         self.name = "REVERSE"
+        self.number = -1
 
 
 class Skip(object):
@@ -10,3 +11,23 @@ class Skip(object):
         self.color = color
         self.type = "SPECIAL"
         self.name = "SKIP"
+        self.number = -2
+
+
+class Wildcard(object):
+    def __init__(self, color):
+        self.color = color
+        self.type = "SPECIAL"
+        self.name = "SKIP"
+        self.number = -2
+        self.is_draw_4 = False
+
+
+class DrawCard:
+    def __init__(self, color):
+        self.color = color
+        self.type = "SPECIAL"
+        self.name = "SKIP"
+        self.number = -2
+        self.color = None
+
